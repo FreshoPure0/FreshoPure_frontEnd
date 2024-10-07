0f089514f319198417e81c6542b94b0c4b75b26f
Mohit Singh Shekhawat <mrshekhawat1228@gmail.com>
2024-10-07 19:52:04 +0530

    working

--- src/store/baseUrl.js
+++ src/store/baseUrl.js
@@ -1,2 +1,2 @@
 // export const baseUrl = "http://3.109.197.98";
-export const baseUrl = "http://192.168.0.32:4000";
+export const baseUrl = "http://192.168.1.2:4000";
--- src/store/reducers/address.js
+++ src/store/reducers/address.js
@@ -32,37 +32,3 @@ export default (state = initialState, action) => {
             return state;
     }
 };
-import { GET_ALL_ADDRESS,GET_SELECTED_ADDRESS,ADD_NEW_ADDRESS,REMOVE_ADDRESS} from '../actions/address';
-
-const initialState = {
-    allAddress:[],
-    selectedAddress : null
-};
-
-export default (state = initialState, action) => {
-    switch(action.type) {
-        case GET_ALL_ADDRESS:
-            return {
-                ...state,
-                allAddress:action.payload.allAddresses,
-                selectedAddress:action.payload.selectedAddress,
-            };
-
-        case REMOVE_ADDRESS:
-            return {
-                ...state, 
-                allAddress:action.payload.allAddresses,
-                selectedAddress:action.payload.selectedAddress,
-            };
-
-        case ADD_NEW_ADDRESS:
-            return {
-                    ...state, 
-                    allAddress:action.payload.allAddresses,
-                    selectedAddress:action.payload.selectedAddress,
-                };
-       
-        default:
-            return state;
-    }
-};
